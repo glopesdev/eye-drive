26702fd8a1b89c8506a35af82cb4ff317abd7fcd
glopesdev <goncaloclopes@gmail.com>
2015-09-09 15:58:55 +0100

    Added session startup script.

---
+++ setup/start-drive.ps1
@@ -0,0 +1,17 @@
+# Get subject name and current date time
+$subjectName = Read-Host "Enter Subject Id"
+$date = [DateTimeOffset]::Now
+
+# Compute session data path
+$dataPath = "..\data"
+$dataPath = Join-Path -Path $dataPath -ChildPath $subjectName
+$dataPath = Join-Path -Path $dataPath -ChildPath ($date).ToString("yyyy_MM_dd-HH_mm")
+
+# Copy protocol control scripts
+New-Item $dataPath -type directory
+copy acquisition.bonsai* $dataPath
+
+# Run bonsai
+cd $dataPath
+Write-Host $dataPath\acquisition.bonsai
+& .\acquisition.bonsai --start
